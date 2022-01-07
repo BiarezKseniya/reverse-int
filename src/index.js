7900a539624d3a93142cce47be0ee79f2268617f
Kseniya <ksenia-2010@tut.by>
2022-01-07 18:34:14 +0300

    feat: add a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let s = '';
+  let length;
+  n = String(Math.abs(n));
+  length = n.length;
+  for (let i = 0; i < length; i++) {
+      s = `${s}${n[length-1-i]}`;
+  }
+  return s;
 }
